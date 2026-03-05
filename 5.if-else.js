0c2f6ac071b762664cc474cc73923467133fe491
Laiane <laiane.s.santos7@ba.estudante.senai.br>
2026-03-05 14:43:48 -0300

    Salvar.

--- 5.if-else.js
+++ 5.if-else.js
@@ -1,13 +1,13 @@
 // Estruturas condicionais.
 
-const idade =2
+const idade =15
 
 if (idade >=18) {
         console.log("Maioridade")
 } else if (idade >= 12) {
         console.log("Adolescente")
-} else { 
-    console.log("Criança")
+}  else if (idade >= 2)  
+   {} else {  console.log("Criança")
 }
 
 // Menor que 2 ano : bebê
