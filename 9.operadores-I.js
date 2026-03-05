187a728f20c36790ebcee1be7a0c20541a185962
Laiane <laiane.s.santos7@ba.estudante.senai.br>
2026-03-05 16:44:17 -0300

    Laço While, Operadores e Funções.

---
+++ 9.operadores-I.js
@@ -0,0 +1,22 @@
+// Operadores logicos
+
+let idade = 10
+let peso = "10"
+
+console.log("- COMPARAÇÃO -")
+console.log("Comparando conteudo:")
+console.log("Idade e peso são diferentes?:")
+console.log(idade == peso)
+
+console.log("\nComparando tipo e conteudo:")
+console.log(idade === peso)
+
+console.log("- Diferença -")
+console.log("\nComparando conteudo:")
+console.log("Idade e peso são diferentes?:")
+console.log(idade != peso)
+
+
+console.log("\nComparando conteudo:")
+console.log("Idade e peso são diferentes?:")
+console.log(idade !== peso)
