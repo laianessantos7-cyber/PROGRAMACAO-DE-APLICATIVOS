97740fd0a8595fbcec8793b379ff4890dab473f7
Laiane <laiane.s.santos7@ba.estudante.senai.br>
2026-03-12 16:43:20 -0300

    Atividade

--- 17.atividade.js
+++ 17.atividade.js
@@ -11,19 +11,19 @@ console.log("Encontre a aluna Marta e mostra o nome e a média dela.")
 const AlunaEncontrada = listaDeAlunos.find(aluna => aluna.nome === "Marta")
 console.log(`Nome: ${AlunaEncontrada.nome} \nMédia: ${AlunaEncontrada.nota}`)
 
-
+console.log("---------------------------")
 console.log("Mostre a média geral da turma.")
 listaDeAlunos.forEach (alunos => {
     console.log(`${alunos.nome} tem a média ${alunos.nota}`)
 })
-
+console.log("---------------------------")
 console.log("Mostre o nome e a média dos alunos com a média abaixo de 7.0.")
 const notabaixa = listaDeAlunos.filter(alunos => alunos.nota < 7)
     notabaixa.forEach(alunos =>
     console.log(`${alunos.nome} tem a média ${alunos.nota}`)
     )
 
-
+console.log("---------------------------")
 console.log("Mostre o nome e a média dos alunos com média maior ou igual a 7.0.")
 const notaalta = listaDeAlunos.filter(alunos => alunos.nota >= 7)
 notaalta.forEach(alunos =>
