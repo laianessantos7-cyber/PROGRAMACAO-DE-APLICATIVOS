04bfb52a21bc015258d8584c5de0a5bebaeca169
Laiane <laiane.s.santos7@ba.estudante.senai.br>
2026-03-26 16:54:07 -0300

    React Js

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,15 @@
+
+import './style.css'
+
+function Mensagem() {
+    return(
+        // Comandos HTML.
+        <div className='mensagem'>
+            <h1>Componente</h1>
+            <p>Este é o componente com uma mensagem simples.</p>
+        </div>
+    )
+    
+}
+
+export default Mensagem
