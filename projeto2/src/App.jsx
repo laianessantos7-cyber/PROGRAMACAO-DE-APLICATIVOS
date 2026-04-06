e0fc8f2c1ab4b659474aa8a31c810efd2b452de7
Aluno <Aluno@C302-LAB-000.senaiba.com.br>
2026-04-06 15:06:57 -0300

    Novo Projeto

---
+++ projeto2/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Welcome!</h1>
+     <h2>My Project.</h2>
+    </>
+  )
+}
+
+export default App
---
+++ projeto2/src/components/AdicionarUsuario/index.jsx
@@ -0,0 +1,55 @@
+import '.style.css/'
+import { useState } from 'react'
+
+export default function AdicionarUsuario() {
+   const [nome, setNome] = useState('')
+   const [email, setEmail] = useState('')
+   const [listaDeUsuarios, setListaDeUsuarios] = useState([])
+
+   // FunçãO para adicionar usuarios em uma lista.
+   const handlerAdicionarUsuario =(event) => {
+    event.preventDefault()
+    if (nome && email){
+        // Adicionar novo usuario.
+
+        setListaDeUsuarios([...listaDeUsuarios, {nome, email}])
+        // Definindo os campos como vazio.
+        setNome ('')
+        setEmail ('')
+    }
+   }
+    return (
+        // Criando HTML da pagina.
+
+        <div className='formulario'>
+            <h2>Adicionar usuario</h2>
+            <form onSubmit= {handlerAdicionarUsuario}>
+                <input 
+                type="text"
+                placeholder='Nome'
+                value={nome}
+                onChange={(e) => setNome (e.target.value)} 
+                />
+
+                <button type='submit'> Adicionar </button>
+                
+            </form>
+
+                <hr />
+
+                <h2>Usuarios Adicionados </h2>
+                <ul>
+                    {/* Usando o map para mostrar cada usuario na lista
+                    com base no indice. */}
+                    {listaDeUsuarios.map((usuario, index) => (
+                    <li key={index}>
+                            {usuario.nome} - {usuario.email}
+                    </li>
+                    ))}
+                    
+                </ul>
+        </div>
+    )
+   
+}
+
